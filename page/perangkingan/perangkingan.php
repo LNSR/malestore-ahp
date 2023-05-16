4557efe91f6494f5e3b0e56fd4d6e0de504c2400
MaulanaSyahrilR <lanasyahrilramadhan@gmail.com>
2023-05-17 00:05:39 +0800

    Tidying code

--- page/perangkingan/perangkingan.php
+++ page/perangkingan/perangkingan.php
@@ -1,41 +1,43 @@
 <?php
-
-$n    = getJumlahKriteria();
+$n = getJumlahKriteria();
 $m = getJumlahAlternatif();
-$matrik = array();
-$matrikb = array();
 
+// Initialize matrices
+$matrik = [];
+$matrikb = [];
+$total = [];
+
+// Fill in the matrices
 for ($x = 1; $x <= $m; $x++) {
   for ($y = 1; $y <= $n; $y++) {
-    $pilih  = getEVAlternatif($x, $y);
+    $pilih = getEVAlternatif($x, $y);
     $matrik[$x][$y] = $pilih;
   }
 }
 
 for ($y = 1; $y <= $n; $y++) {
-  $pilih  = getKriteriaPV($y);
+  $pilih = getKriteriaPV($y);
   $matrikb[$y] = $pilih;
 }
 
-// inisialisasi jumlah tiap kolom dan baris kriteria
-$total = array();
+// Calculate the total for each row in the criteria table
 for ($i = 0; $i <= ($n - 1); $i++) {
   $total[$i] = 0;
 }
 
-// menghitung jumlah pada baris kriteria tabel nilai kriteria
-// matrikb merupakan matrik yang telah dinormalisasi
 for ($x = 1; $x <= $m; $x++) {
   for ($y = 1; $y <= $n; $y++) {
-    $value  = $matrik[$x][$y] * $matrikb[$y];
+    $value = $matrik[$x][$y] * $matrikb[$y];
     $total[$x] += $value;
   }
 }
 ?>
+<!-- Content -->
 <section class="content">
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-12">
+        <!-- Tabel Bobot Karyawan -->
         <div class="card">
           <div class="card-header">
             <h3 class="card-title">Tabel Bobot Karyawan</h3>
@@ -46,27 +48,27 @@ for ($x = 1; $x <= $m; $x++) {
                 <thead>
                   <tr>
                     <th>Karyawan</th>
-                    <?php
-                    for ($i = 0; $i <= ($n - 1); $i++) {
-                      echo "<th>" . getKriteriaNama($i) . "</th>";
-                    } ?>
+                    <?php for ($i = 0; $i <= ($n - 1); $i++) { ?>
+                      <th><?= getKriteriaNama($i) ?></th>
+                    <?php } ?>
                   </tr>
                 </thead>
                 <tbody>
-                  <?php
-                  for ($x = 1; $x <= $m; $x++) {
-                    echo "<tr>";
-                    echo "<th>" . getAlternatifNama($x - 1) . "</th>";
-                    for ($y = 1; $y <= $n; $y++) {
-                      echo "<td>" . number_format(getEVAlternatif($x, $y), 3) . "</td>";
-                    }
-                    echo "</tr>";
-                  } ?>
+                  <?php for ($x = 1; $x <= $m; $x++) { ?>
+                    <tr>
+                      <th><?= getAlternatifNama($x - 1) ?></th>
+                      <?php for ($y = 1; $y <= $n; $y++) { ?>
+                        <td><?= number_format(getEVAlternatif($x, $y), 3) ?></td>
+                      <?php } ?>
+                    </tr>
+                  <?php } ?>
                 </tbody>
               </table>
             </form>
           </div>
         </div>
+
+        <!-- Tabel Bobot Kriteria -->
         <div class="card">
           <div class="card-header">
             <h3 class="card-title">Tabel Bobot Kriteria</h3>
@@ -76,18 +78,16 @@ for ($x = 1; $x <= $m; $x++) {
               <table class="table table-bordered">
                 <thead>
                   <tr>
-                    <?php
-                    for ($i = 0; $i <= ($n - 1); $i++) {
-                      echo "<th>" . getKriteriaNama($i) . "</th>";
-                    } ?>
+                    <?php for ($i = 0; $i <= ($n - 1); $i++) { ?>
+                      <th><?= getKriteriaNama($i) ?></th>
+                    <?php } ?>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <?php
-                    for ($x = 1; $x <= $n; $x++) {
-                      echo "<td>" . number_format(getEVKriteria($x), 3) . "</td>";
-                    } ?>
+                    <?php for ($x = 1; $x <= $n; $x++) { ?>
+                      <td><?= number_format(getEVKriteria($x), 3) ?></td>
+                    <?php } ?>
                   </tr>
                 </tbody>
               </table>
@@ -95,55 +95,57 @@ for ($x = 1; $x <= $m; $x++) {
           </div>
         </div>
 
+        <!-- Tabel Ranking -->
         <div class="card">
-  <div class="card-header">
-    <h3 class="card-title">Tabel Ranking</h3>
-  </div>
-  <div class="card-body">
-    <form method="POST">
-      <table class="table table-bordered text-center">
-        <thead>
-          <tr>
-            <th>Ranking</th>
-            <th>Karyawan</th>
-            <?php
-            for ($i = 0; $i <= ($n - 1); $i++) {
-              echo "<th>" . getKriteriaNama($i) . "</th>";
-            } ?>
-            <th bgcolor="#41fc03">TOTAL</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          // Kalkulasi masing-masing total score karyawan(alternatif)
-          for ($x = 1; $x <= $m; $x++) {
-            $total[$x] = 0;
-            for ($y = 1; $y <= $n; $y++) {
-              $total[$x] += ($matrik[$x][$y] * $matrikb[$y]);
-            }
-          }
+          <div class="card-header">
+            <h3 class="card-title">Tabel Ranking</h3>
+          </div>
+          <div class="card-body">
+            <form method="POST">
+              <table class="table table-bordered text-center">
+                <thead>
+                  <tr>
+                    <th>Ranking</th>
+                    <th>Karyawan</th>
+                    <?php for ($i = 0; $i <= ($n - 1); $i++) { ?>
+                      <th><?= getKriteriaNama($i) ?></th>
+                    <?php } ?>
+                    <th bgcolor="#41fc03">TOTAL</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  // Kalkulasi masing-masig total skor karyawan
+                  for ($x = 1; $x <= $m; $x++) {
+                    $total[$x] = 0;
+                    for ($y = 1; $y <= $n; $y++) {
+                      $total[$x] += ($matrik[$x][$y] * $matrikb[$y]);
+                    }
+                  }
 
-          // Display the alternatives in descending order of score
-          arsort($total);
-          $ranking = 1;
-          foreach ($total as $key => $value) {
-            if ($value > 0) {
-              echo "<tr>";
-              echo "<td>" . $ranking++ . "</td>";
-              echo "<th>" . getAlternatifNama($key - 1) . "</th>";
-              for ($y = 1; $y <= $n; $y++) {
-                echo "<td>" . number_format(($matrik[$key][$y] * $matrikb[$y]), 4) . "</td>";
-              }
-              echo "<td bgcolor='#41fc03'>" . number_format(($total[$key]), 4) . "</td>";
-              echo "</tr>";
-            }
-          }
-          ?>
-        </tbody>
-      </table>
-    </form>
-  </div>
-</div>
+                  // Tampilkan Skor karyawan
+                  arsort($total);
+                  $ranking = 1;
+                  foreach ($total as $key => $value) {
+                    if ($value > 0) {
+                      ?>
+                      <tr>
+                        <td><?= $ranking++ ?></td>
+                        <th><?= getAlternatifNama($key - 1) ?></th>
+                        <?php for ($y = 1; $y <= $n; $y++) { ?>
+                          <td><?= number_format(($matrik[$key][$y] * $matrikb[$y]), 4) ?></td>
+                        <?php } ?>
+                        <td bgcolor='#41fc03'><?= number_format(($total[$key]), 4) ?></td>
+                      </tr>
+                    <?php
+                    }
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
